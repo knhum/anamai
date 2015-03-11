c2082ae3b0152725e5d123c0ad90eb0a7f29d0ae
Student <Student@10.8.102.86>
2015-03-11 15:30:04 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,5 +1,18 @@
 <?php
+use yii\helpers\Html;
 /* @var $this yii\web\View */
 $this->title = 'กรมอนามัย';
 ?>
-abcdefghijklmnopqrstuvwxyz
+<?php
+$route1=Yii::$app->urlManager->createUrl('test/test1');
+?>
+<a href="<?=$route1?>">ไปที่ test1</a><br>
+
+<?php
+$route2=Yii::$app->urlManager->createUrl(['test/test2','name'=>'kreangsak','lname'=>'sanchai']);
+?>
+<a href="<?=$route2?>">ไปที่ test2</a><br>
+
+<?=
+Html::a('ลิงค์แบบที่ 3',['test/test1','a'=>'1']);
+?>
